de5bf5066a997cdf1fcb68cc57263734fe1fbccd
kcavs <kcav1321@gmail.com>
2024-02-08 15:00:11 -0500

    go

--- ex_3.java
+++ ex_3.java
@@ -1,3 +1,8 @@
 public class ex_3 {
+    public static void main(String[]args){
+        System.out.println("Kiersten Cavalli");
+        System.out.println("\nsong: whats with these homies dissing my girl?");
+        System.out.println("poem: my heart is filled with love but my tummy is filled with dirt (picked by adelle)");
+    }
     
 }
