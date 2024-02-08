17794cfebaa8e038e632cacc2cd3de068f38329e
kcavs <kcav1321@gmail.com>
2024-02-08 14:47:55 -0500

    i did it!!

---
+++ ex_2.java
@@ -0,0 +1,10 @@
+public class ex_2 {
+    public static void main(String[]args){
+        System.out.println("*********************************");
+        System.out.println("*                               *");
+        System.out.println("*        Sue's Swimwear!        *");
+        System.out.println("*Email: SwimWithSue@swimwear.com*");
+        System.out.println("*                               *");
+        System.out.println("*********************************");
+    }
+}
---
+++ ex_3.java
@@ -0,0 +1,3 @@
+public class ex_3 {
+    
+}
